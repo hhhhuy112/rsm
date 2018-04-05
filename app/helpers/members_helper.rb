c6374d8d591d20d3f19bfd7883684e089d6cce21
lamphan <dinhlam29995@gmail.com>
2018-04-05 15:15:43 +0700

    fix bug datepicker and site members

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -2,4 +2,8 @@ module MembersHelper
   def is_checked? member_checkeds, user
     member_checkeds.include? user.id.to_s
   end
+
+  def get_role_member
+    Member.roles.map{|key, value| [t("member.roles.#{key}"), key]}
+  end
 end
