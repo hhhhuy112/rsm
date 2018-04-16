ce17c230f9a5ddf303a5aa55174ce802d50fecd1
lamphan <dinhlam29995@gmail.com>
2018-04-16 11:44:00 +0700

    edit join_at and view show note

--- app/assets/javascripts/channels/notify_send_mail.js
+++ app/assets/javascripts/channels/notify_send_mail.js
@@ -10,13 +10,15 @@ $(document).ready(function() {
         current_user = parseInt($('#current-user-id').val());
         if (data.user === current_user)
         {
-          if ($('#status-email-' + data.id).length === 0) {
             $counter = $('.counter-notify-mail').text();
             val = parseInt($counter);
+          if ($('#status-email-' + data.id).length === 0) {
             val++;
             $('.counter-notify-mail').text(val);
             $('.notify-mail-list').prepend(data.notify_mail);
           } else {
+            val--;
+            $('.counter-notify-mail').text(val);
             $('#status-email-' + data.id).attr('class', 'time pull-right color-success');
             $('#status-email-' + data.id).attr('id', 'status-email-' + data.id);
             $('#status-email-' + data.id).text(I18n.t('employers.email_sents.success'));
