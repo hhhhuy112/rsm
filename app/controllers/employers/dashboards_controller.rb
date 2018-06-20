78cad870f623a0f37cb023c294fdd525aa07d8e7
VoTrung96 <votiensy240196@gmail.com>
2018-06-20 11:39:19 +0700

    display skills

--- app/controllers/employers/dashboards_controller.rb
+++ app/controllers/employers/dashboards_controller.rb
@@ -8,12 +8,6 @@ class Employers::DashboardsController < Employers::EmployersController
 
   private
 
-  def check_permissions_employer
-    return if current_user.is_employer_of? @company.id
-    flash[:danger] = t "company_mailer.fail"
-    redirect_to root_url
-  end
-
   def check_params
     if params[:q].present? && params[:q][:created_at_gteq].present? &&
       params[:q][:created_at_lteq].present? && params[:q][:created_at_lteq].to_date &&
