37e6c3c37f8140f8448a6864781fafb3cfc1185e
hhhhuy112 <minhhuyho2011@gmail.com>
2018-07-03 17:27:02 +0700

    fix bug password

--- app/controllers/devises/confirmations_controller.rb
+++ app/controllers/devises/confirmations_controller.rb
@@ -1,6 +1,22 @@
 class Devises::ConfirmationsController < Devise::ConfirmationsController
   skip_before_action :verify_authenticity_token, only: :create, if: ->{request.xhr?}
 
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    if resource.errors.empty?
+      set_flash_message(:notice, :confirmed) if is_navigational_format?
+      sign_in(resource_name, resource)
+      respond_with_navigational(resource){ redirect_to after_sign_in_path_for(resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+
+  end
+
+  def new
+    super
+  end
+
   def create
     if request.xhr?
       user = User.find_by email: params[:email]
--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,4 +2,9 @@ class DeviseMailer < Devise::Mailer
   layout "devise_mailer"
 
   add_template_helper EmailHelper
+
+  def confirmation_instructions(record, token, opts={})
+    super
+  end
+
 end
