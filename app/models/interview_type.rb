dc6c6262084f284b0ab65978d3508a9014ce0174
VoTrung96 <votiensy240196@gmail.com>
2018-07-12 15:27:49 +0700

    create interview_type

--- app/models/interview_type.rb
+++ app/models/interview_type.rb
@@ -6,5 +6,7 @@ class InterviewType < ApplicationRecord
   has_many :skill_sets, dependent: :destroy
   has_many :skills, through: :skill_sets
 
+  accepts_nested_attributes_for :skill_sets, reject_if: :all_blank
+
   validates :name, presence: true, uniqueness: true
 end
